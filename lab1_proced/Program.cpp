f46d84ffe92c4f95a142cf8bff573c39bdf78857
return-Milkyway <66420810+return-Milkyway@users.noreply.github.com>
2020-06-04 19:00:22 +0700

    Add files via upload

---
+++ lab1_proced/Program.cpp
@@ -0,0 +1,165 @@
+#include "Program.h"
+using namespace std;
+	 
+struct container* In(container *lst,ifstream &ifst){
+		struct container *last = Init2(In(ifst));
+		struct container *tmp;
+		tmp = lst->next; 
+		lst->next = last; 
+		last->next = tmp; 
+		lst->prev=last;
+		last->prev=lst;
+	while(!ifst.eof()) {			
+		  struct container *newItem = Init2(In(ifst));
+		  struct container *tmp;
+		  struct container *tmp2;		  
+		  tmp = last->next; 
+		  last->next = newItem;
+		  newItem->next = tmp;
+		  newItem->prev=last;
+		  lst->prev=newItem;
+		  last=newItem;
+		}
+		return(last);
+}
+ 	
+void In(diagol &a, ifstream &ifst) {
+	ifst.get();
+	ifst>>a.y;
+ 	a.x=new int*;
+ 	for (int count=0;count<a.y;count++){
+ 		a.x[count]=new int[a.y];
+ 	}
+	for (int count=0;count<a.y;count++){
+ 		ifst>>	a.x[count][count];
+ 	}
+}
+ 
+  void In(square &p, ifstream &ifst){
+	ifst.get();
+	p.a=new int*;
+	ifst>>p.b;
+	for (int count=0;count<p.b;count++){
+ 		p.a[count]=new int[p.b];
+ 	}
+	for (int count=0;count<p.b;count++){
+ 		for (int count2=0;count2<p.b;count2++){
+ 		ifst>>	p.a[count][count2];
+ 	}
+ 	}
+ }
+ 
+ matr* In(ifstream &ifst){
+ 	matr *sp;
+ 	int k;
+ 	ifst >> k;
+ 	switch(k) {
+ 		case 1:
+ 			sp = new matr({});
+ 			sp->k = matr::key::SQUARE;
+ 			In(sp->s, ifst);
+ 			return sp;
+ 		case 2:
+ 			sp = new matr({});
+ 			sp->k = matr::key::DIAGOL;
+ 			In(sp->d, ifst);
+ 			return sp;
+ 		default:
+ 		return 0;
+ 	}
+ }
+ 
+ void Out(square &p, ofstream &ofst){
+	ofst << "It is Square matrix: len = "
+	 << p.b << ", matr = " << endl;
+	 for (int count=0;count<p.b;count++){
+		for (int count2=0;count2<p.b;count2++){
+			ofst<<p.a[count][count2]<<" ";
+		}
+		ofst<<endl;
+	}
+ }
+ 
+  void Out(diagol &a, ofstream &ofst) {
+ 	ofst << "It is Diagol Matrix: len = " << a.y
+	<< ", matr: = " <<endl;
+	for (int count=0;count<a.y;count++){
+		for (int count2=0;count2<a.y;count2++){
+			if (count!=count2){
+				ofst<<"0 ";
+			}
+			else{
+			ofst<<a.x[count][count]<<" ";
+			}
+		}
+		ofst<<endl;
+	}
+ }
+ 
+ void Out(matr &s, ofstream &ofst) {
+	 switch(s.k) {
+	 	case matr::key::SQUARE:
+	 		Out(s.s, ofst);
+	 		break;
+	 	case matr::key::DIAGOL:
+	 		Out(s.d, ofst);
+	 		break;
+	 	default:
+	 		ofst << "Incorrect figure!" << endl;
+ 	};
+ };
+ 
+ void Out(container *lst,ofstream &ofst){ 
+	struct container *p;
+	p=lst;
+	int num=0;
+	  do {
+	    num=num+1;
+	    p = p->next; 
+	  } while (p != lst); 
+	  ofst<<"Container contains " << num-1 	<< " elements." << endl;
+  	if(lst->next==lst){
+		return;
+	}
+  	p = lst->next;
+	  do {
+	    matr *s=p->cont;
+	    Out(*s,ofst);
+	    p = p->next; 
+	  } while (p != lst); 
+}
+
+struct container *  Init(){
+	struct container *c = new container;
+  c->next = c;
+  c->prev=c; 
+  return c;
+}
+struct container *  Init2(matr  *a) {
+  struct container *c = new container;
+  c->cont = a;
+  c->next = c;
+  c->prev=c; 
+  return c;
+}
+
+ struct container * Clear(container *lst){
+	struct container *p;
+	p=lst->next;
+  do 
+  {
+	struct container *temp;
+	temp = p;
+	while (temp->next != p) { 
+			temp = temp->next;
+		}
+  	temp->next = p->next; 
+	p = p->next; // переход к следующему узлу
+  } while (p != lst); // условие окончания обхода
+
+}
+
+ 
+ 
+ 
+ 
---
+++ lab1_proced/Program.h
@@ -0,0 +1,52 @@
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+ 	struct square 
+	{
+		int b;
+		int** a;
+ 	};
+ 
+ 	struct diagol 
+	{
+		int y;
+		int** x;
+  	};
+  	
+	struct matr {
+	enum  key{SQUARE, DIAGOL};
+	key k;    
+	union 
+	{ 
+		square s;
+		diagol d;
+	};
+};
+	
+	 struct container
+ 	{
+ 		matr *cont;
+ 		struct container *next;
+ 		struct container *prev;
+ 	};
+ 	
+ 	struct container* In(container *lst,ifstream &ifst);
+	void In(square &a, ifstream &ifst);
+	void In(diagol &p, ifstream &ifst);
+	matr* In(ifstream &ifst);
+	void Out(diagol &p, ofstream &ofst);
+	void Out(square &a,ofstream &ofst);
+	void Out(matr &m, ofstream &ofst);
+	void Out(container *lst,ofstream &ofst);
+	struct container *  Init();
+	struct container *  Init2(matr  *a);
+	struct container * Clear(container *lst);
+
+ 	
+	
+
+
+ 
