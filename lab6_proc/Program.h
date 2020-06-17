26619999bdc226b40f788546f398b665d247cb77
Pechenka-ovsynka <66328619+Pechenka-ovsynka@users.noreply.github.com>
2020-06-18 04:10:59 +0700

    Add files via upload
    some fix

--- lab6_proc/Program.h
+++ lab6_proc/Program.h
@@ -1,17 +1,9 @@
-
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 using namespace std;
 
-bool FileIsExist2(string filePath){
-    bool isExist = false;
-    std::ifstream fin(filePath.c_str());
-    if(fin.is_open())
-        isExist = true;
-    fin.close();
-    return isExist;
-}
+bool FileIsExist(string filePath);
 
 struct square {
 	int razm;
@@ -46,17 +38,17 @@ struct container {
  	struct container *prev;
 };
 
-int In(container *lst,ifstream &ifst);
+int In(container *head,ifstream &ifst);
 int In(square &s, ifstream &ifst);
 int In(diagol &d, ifstream &ifst);
-int In(down_triangle &dt,ofstream &ofst);
+int In(down_triangle &dt,ifstream &ifst);
 matr* In(ifstream &ifst);
 void Out(diagol &d, ofstream &ofst);
 void Out(square &s,ofstream &ofst);
 void Out(down_triangle &dt,ofstream &ofst);
 void Out(matr &mtrx, ofstream &ofst);
-void Out(container *lst,ofstream &ofst);
-void Out_Square(container *lst,ofstream &ofst);
+void Out(container *head,ofstream &ofst);
+void Out_Square(container *head,ofstream &ofst);
 struct container * Init();
 struct container * Init2(matr  *a);
 struct container * Clear(container *lst);
@@ -64,7 +56,7 @@ int Sum(diagol* d);
 int Sum(down_triangle* dt);
 int Sum(square* s);
 int Sum(matr *mtrx);
-void Out_Sum(container *lst,ofstream &ofst);
+void Out_Sum(container *head,ofstream &ofst);
 bool Compare(matr *first, matr *second);
 int Sort(container* c);
 struct container * Swap(struct container *lst1, struct container *lst2, struct container *head);
