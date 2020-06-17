ea64c230bfcb829bf93e711d4a1541abe718ea93
return-Milkyway <66420810+return-Milkyway@users.noreply.github.com>
2020-06-17 21:09:57 +0700

    Add files via upload

---
+++ lab6_proc/Program.h
@@ -0,0 +1,73 @@
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+bool FileIsExist2(string filePath){
+    bool isExist = false;
+    std::ifstream fin(filePath.c_str());
+    if(fin.is_open())
+        isExist = true;
+    fin.close();
+    return isExist;
+}
+
+struct square {
+	int razm;
+	int** arr;
+};
+ 
+struct diagol {
+	int razm;
+	int** arr;
+};
+
+struct down_triangle {
+	int razm;
+	int* arr;
+};
+
+struct matr {
+	enum  key{SQUARE, DIAGOL,DOWN_TRIANGLE};
+	key k;    
+	union { 
+		square s;
+		diagol d;
+		down_triangle dt;
+	};
+	enum var_print{POSTROCHNO,POSTOLBZAM,ODNOMERNO,INCORRECT};//1,2,3
+	var_print variant;
+};
+
+struct container {
+ 	matr *cont;
+ 	struct container *next;
+ 	struct container *prev;
+};
+
+int In(container *lst,ifstream &ifst);
+int In(square &s, ifstream &ifst);
+int In(diagol &d, ifstream &ifst);
+int In(down_triangle &dt,ofstream &ofst);
+matr* In(ifstream &ifst);
+void Out(diagol &d, ofstream &ofst);
+void Out(square &s,ofstream &ofst);
+void Out(down_triangle &dt,ofstream &ofst);
+void Out(matr &mtrx, ofstream &ofst);
+void Out(container *lst,ofstream &ofst);
+void Out_Square(container *lst,ofstream &ofst);
+struct container * Init();
+struct container * Init2(matr  *a);
+struct container * Clear(container *lst);
+int Sum(diagol* d);
+int Sum(down_triangle* dt);
+int Sum(square* s);
+int Sum(matr *mtrx);
+void Out_Sum(container *lst,ofstream &ofst);
+bool Compare(matr *first, matr *second);
+int Sort(container* c);
+struct container * Swap(struct container *lst1, struct container *lst2, struct container *head);
+
+
+ 
